c170178c46e9d22b43a8287ee65cf758e3a01d57
Pavel Witassek <pavel.witassek@gmail.com>
2018-03-29 11:32:25 +0200

    Odstranena identity
    Ponechani id jako INT, at se na to daji udelat triggery v poslednim
    ukolu

--- xwitas00_xvanic09.sql
+++ xwitas00_xvanic09.sql
@@ -10,21 +10,22 @@ DROP TABLE Typ_Letadla;
 DROP TABLE Terminal;
 
 CREATE TABLE Terminal (
-	id_terminalu	INT GENERATED ALWAYS AS IDENTITY,
+	id_terminalu	INT NOT NULL,
 	nazev			VARCHAR(200) NOT NULL,
 	CONSTRAINT PK_Terminal PRIMARY KEY (id_terminalu),
     CONSTRAINT UQ_TerminalNazev UNIQUE (nazev)
 );
 
 CREATE TABLE Typ_Letadla (
-    id_typu     	INT GENERATED ALWAYS AS IDENTITY,
+    id_typu     	INT NOT NULL,
     vyrobce     	VARCHAR(200),
     nazev       	VARCHAR(200) NOT NULL,
     CONSTRAINT PK_Typ_Letadla PRIMARY KEY (id_typu)
+    -- nazev neni unique, protoze 2 vyrobci mohou mit stejny nazev typu letadla
 );
 
 CREATE TABLE Letadlo (
-    id_letadla      INT GENERATED ALWAYS AS IDENTITY,
+    id_letadla      INT NOT NULL,
     pocet_posadky   INT NOT NULL,
     datum_vyroby    DATE NOT NULL,
     datum_revize    DATE NOT NULL,
@@ -34,14 +35,14 @@ CREATE TABLE Letadlo (
 );
 
 CREATE TABLE Trida (
-	id_tridy		INT GENERATED ALWAYS AS IDENTITY,
+	id_tridy		INT NOT NULL,
 	nazev			VARCHAR(200) NOT NULL,
 	CONSTRAINT	PK_Trida PRIMARY KEY (id_tridy)
     -- UNIQUE nazev??
 );
 
 CREATE TABLE Gate (
-	id_gate			INT GENERATED ALWAYS AS IDENTITY,
+	id_gate			INT NOT NULL,
 	nazev			VARCHAR(200) NOT NULL,
 	id_terminalu	INT NOT NULL,
 	CONSTRAINT FK_GateTerminal FOREIGN KEY (id_terminalu) REFERENCES Terminal (id_terminalu),
@@ -50,7 +51,7 @@ CREATE TABLE Gate (
 );
 
 CREATE TABLE Let (
-    id_letu         INT GENERATED ALWAYS AS IDENTITY,
+    id_letu         INT NOT NULL,
     datum_odletu    DATE NOT NULL,
     cas_odletu      TIMESTAMP NOT NULL,
     doba_letu       TIMESTAMP NOT NULL,
@@ -63,7 +64,7 @@ CREATE TABLE Let (
 );
 
 CREATE TABLE Misto (
-	id_mista		INT GENERATED ALWAYS AS IDENTITY,
+	id_mista		INT NOT NULL,
 	cislo_mista		INT NOT NULL,
 	umisteni		VARCHAR(1) NOT NULL,
 	id_letadla		INT NOT NULL,
@@ -107,24 +108,24 @@ CREATE TABLE Typ_LetadlaGate (
 
 
 -- VLOZENI UKAZKOVYCH DAT
-INSERT INTO Terminal (nazev) VALUES ('Terminal Pepa');
-INSERT INTO Terminal (nazev) VALUES ('Terminal Franta');
+INSERT INTO Terminal (id_terminalu, nazev) VALUES (1, 'Terminal Pepa');
+INSERT INTO Terminal (id_terminalu, nazev) VALUES (2, 'Terminal Franta');
 
-INSERT INTO Gate (nazev, id_terminalu) VALUES ('1A', 1);
-INSERT INTO Gate (nazev, id_terminalu) VALUES ('1B', 1);
-INSERT INTO Gate (nazev, id_terminalu) VALUES ('1C', 1);
-INSERT INTO Gate (nazev, id_terminalu) VALUES ('2A', 1);
-INSERT INTO Gate (nazev, id_terminalu) VALUES ('2B', 1);
-INSERT INTO Gate (nazev, id_terminalu) VALUES ('1A', 2);
-INSERT INTO Gate (nazev, id_terminalu) VALUES ('1B', 2);
-INSERT INTO Gate (nazev, id_terminalu) VALUES ('1C', 2);
-INSERT INTO Gate (nazev, id_terminalu) VALUES ('1D', 2);
-INSERT INTO Gate (nazev, id_terminalu) VALUES ('2A', 2);
-INSERT INTO Gate (nazev, id_terminalu) VALUES ('2B', 2);
+INSERT INTO Gate (id_gate, nazev, id_terminalu) VALUES (1, '1A', 1);
+INSERT INTO Gate (id_gate, nazev, id_terminalu) VALUES (2, '1B', 1);
+INSERT INTO Gate (id_gate, nazev, id_terminalu) VALUES (3, '1C', 1);
+INSERT INTO Gate (id_gate, nazev, id_terminalu) VALUES (4, '2A', 1);
+INSERT INTO Gate (id_gate, nazev, id_terminalu) VALUES (5, '2B', 1);
+INSERT INTO Gate (id_gate, nazev, id_terminalu) VALUES (6, '1A', 2);
+INSERT INTO Gate (id_gate, nazev, id_terminalu) VALUES (7, '1B', 2);
+INSERT INTO Gate (id_gate, nazev, id_terminalu) VALUES (8, '1C', 2);
+INSERT INTO Gate (id_gate, nazev, id_terminalu) VALUES (9, '1D', 2);
+INSERT INTO Gate (id_gate, nazev, id_terminalu) VALUES (10, '2A', 2);
+INSERT INTO Gate (id_gate, nazev, id_terminalu) VALUES (11, '2B', 2);
 
-INSERT INTO Typ_letadla (nazev, vyrobce) VALUES ('747', 'Boeing');
-INSERT INTO Typ_letadla (nazev, vyrobce) VALUES ('A320', 'Airbus');
+INSERT INTO Typ_letadla (id_typu, nazev, vyrobce) VALUES (1, '747', 'Boeing');
+INSERT INTO Typ_letadla (id_typu, nazev, vyrobce) VALUES (2, 'A320', 'Airbus');
 
-INSERT INTO Trida (nazev) VALUES ('Economy');
-INSERT INTO Trida (nazev) VALUES ('Business');
-INSERT INTO Trida (nazev) VALUES ('Premium');
+INSERT INTO Trida (id_tridy, nazev) VALUES (1, 'Economy');
+INSERT INTO Trida (id_tridy, nazev) VALUES (2, 'Business');
+INSERT INTO Trida (id_tridy, nazev) VALUES (3, 'Premium');
